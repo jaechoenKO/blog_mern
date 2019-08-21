fad3c33e0350a4208a62017d7ac915f5d1be1db4
jaecheon <jjocojusu1@gmail.com>
2019-08-21 21:43:49 +0900

    add test routes

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 
+const postsRouter = require('./routes/api/posts');
+const profileRouter = require('./routes/api/profile');
+const usersRouter = require('./routes/api/users');
+
 const PORT = process.env.PORT || 3000;
+
+app.use('/posts', postsRouter);
+app.use('/profile', profileRouter);
+app.use('/users', usersRouter);
+
+
+
+// env 서버의 중요한 자료 모아둠
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
