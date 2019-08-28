fe48a05651ccb89fec57cedc7efb0dd3c1478e54
jaecheon <jjocojusu1@gmail.com>
2019-08-28 11:56:35 +0900

    add comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+/**
+ * @route   GET posts/test
+ * @desc    tests posts route
+ * @access  Public
+ */
 router.get('/test', (req, res) => {
     res.status(200).json({
         msg: "posts works"
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+/**
+ * @route   GET profile/test
+ * @desc    tests profile route
+ * @access  Public
+ */
 router.get('/test', (req, res) => {
     res.status(200).json({
         msg: "profile works"
